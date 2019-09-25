ef9070035d5a790cde38d36e3346715b073a0924
User <User@DESKTOP-2RC2UP5>
2019-09-25 07:38:34 +0700

    first commit

---
+++ src/latihan4/Latihan4.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan4;
+/**
+ *
+ * @author User
+ */
+public class Latihan4 {
+    
+    
+    public void tambah(){
+        //variabel lokal
+         int jumlahKambing = 0;
+         jumlahKambing = jumlahKambing +15;
+         System.out.println("Jumlah kambing setelah di tambah:"+
+                 jumlahKambing);
+        
+    }
+    
+            
+
+  
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Latihan4 kambingJantan = new Latihan4();
+        kambingJantan.tambah();
+    }
+    
+}
